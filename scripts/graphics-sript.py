24439c1ad2509c769a66f84aff9a56db227407a5
tolik <talamtolik@gmail.com>
2018-01-05 20:26:54 +0300

    update info

--- scripts/graphics-sript.py
+++ scripts/graphics-sript.py
@@ -15,13 +15,13 @@ i = lower_bound
 while(i <= upper_bound):
     X.append(i)
     i += 10000
-      
+
 # Набор значений по оси Y
 
-Y_B  = []
-Y_Q  = []
-Y_OB = []
-
+Y_B   = []
+Y_Q   = []
+Y_OB  = []
+Y_OBS = []
 
 f_b = open('log_bucket_sort.txt', 'r')
 for line in f_b:
@@ -40,12 +40,18 @@ for line in f_bo:
 	Y_OB.append(float(line))
 
 f_bo.close()
+
+f_bos = open('log_opt_bucket_sort_std_sort.txt', 'r')
+for line in f_bos:
+	Y_OBS.append(float(line))
+
+f_bos.close()
 # Строим диаграмму
 # Задаем исходные данные для каждой линии диаграммы, внешний вид линий и маркеров.
 # Функция plot() возвращает кортеж ссылок на объекты класса matplotlib.lines.Line2D
 
 
-line_b, line_q, line_ob = plt.plot(X, Y_B, 'b-', X, Y_Q, 'r-', X, Y_OB, 'g-'  )
+line_b, line_q, line_ob, line_obs = plt.plot(X, Y_B, 'b-', X, Y_Q, 'r-', X, Y_OB, 'g-', X, Y_OBS, 'y-'  )
 
 
 # Задаем интервалы значений по осям X и Y
@@ -63,7 +69,7 @@ plt.ylabel(u'Время, ms ')
 
 # Задаем исходные данные для легенды и ее размещение
 
-plt.legend((line_b, line_q, line_ob), (u'Bucket-Sort', u'STL-Sort', u'OPT-Bucket-Sort'), loc = 'best')
+plt.legend((line_b, line_q, line_ob, line_obs), (u'Bucket-Sort', u'STL-Sort', u'OPT-Bucket-Sort', u'OPT-Bucket-Sort-Std-Sort-Bucket'), loc = 'best')
 
 # Включаем сетку
 
