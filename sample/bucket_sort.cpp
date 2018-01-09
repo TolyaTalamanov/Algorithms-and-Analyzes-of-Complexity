e9b8b03f80e1d2f9fab7c067dcd201269409047a
tolik <talamtolik@gmail.com>
2018-01-09 17:22:43 +0300

    gauss disribution

--- sample/bucket_sort.cpp
+++ sample/bucket_sort.cpp
@@ -16,22 +16,22 @@ int main(int argc, char** argv) {
 
     mt19937 generator(seed);
     vector<double> data(size);
-    if(!argv[3]){
-        std::cout << "Enter distribution" << '\n';
-        exit(-1);
+    if(argv[3] != nullptr){
+        if(string(argv[3]) == "g"){
+            normal_distribution<> distribution(0.5, 0.1);
+            generate(data.begin(), data.end(), [&distribution, &generator](){
+                return distribution(generator);
+            });
+        }
+        else{
+            uniform_real_distribution<double> distribution(0, 1);
+            generate(data.begin(), data.end(), [&distribution, &generator](){
+                return distribution(generator);
+            });
+        }
     }
-    if(string(argv[3])  == "g"){
-        normal_distribution<> distribution(0.5, 0.2);
-        generate(data.begin(), data.end(), [&distribution, &generator](){
-            return distribution(generator);
-        });
-    }
-
-    if(string(argv[3])  == "u"){
-        uniform_real_distribution<double> distribution(0, 1);
-        generate(data.begin(), data.end(), [&distribution, &generator](){
-            return distribution(generator);
-        });
+    else{
+        std::cout << "enter disribution" << '\n';
     }
     // cout << "before sort : " << endl;
     // copy(data.begin(), data.end(), ostream_iterator<double>(cout, " "));
--- sample/opt_bucket_sort.cpp
+++ sample/opt_bucket_sort.cpp
@@ -15,16 +15,35 @@ int main(int argc, char** argv) {
         cerr << "Enter size and seed" << endl;
 
     mt19937 generator(seed);
-    uniform_real_distribution<double> distribution(0, 1);
     vector<double> data(size);
-
-    generate(data.begin(), data.end(), [&distribution, &generator](){
-        return distribution(generator);
-    });
-
+    if(argv[3] != nullptr){
+        if(string(argv[3]) == "g"){
+            std::cout << "gauss" << '\n';
+            normal_distribution<> distribution(0.5, 0.3);
+            generate(data.begin(), data.end(), [&distribution, &generator](){
+                double randVal;
+                do{
+                    randVal = distribution(generator);
+                }while(randVal > 1 || randVal < 0);
+                return randVal;
+            });
+        }
+        else{
+            std::cout << "uniform" << '\n';
+            uniform_real_distribution<double> distribution(0, 1);
+            generate(data.begin(), data.end(), [&distribution, &generator](){
+                return distribution(generator);
+            });
+        }
+    }
+    else{
+        std::cout << "enter disribution" << '\n';
+    }
+    std::cout << "size array = " << data.size() << '\n';
     auto start = Time::now();
     opt_bucket_sort(data.begin(), data.end());
     auto end   = Time::now();
+    // copy(data.begin(), data.end(), ostream_iterator<double>(cout, " "));
     fms time = end - start;
     cout << time.count() << '\n';
 
